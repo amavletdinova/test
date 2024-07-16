82ecd08f6ba4017d33d9297f3e86f64477f982a0
amavletdinova <165006777+amavletdinova@users.noreply.github.com>
2024-07-16 12:04:40 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,14 +1,15 @@
 <?
+if ($_POST["send"]) {
+include_once "smsc_api.php";
 $client = new SoapClient ("http://smsc.ru/sys/soap.php?wsdl");
 $ret = $client->send_sms(array("login"=>"alex", "psw"=>"123", $_POST["phone"], $_POST["message"], 0, $_POST["sender"]));
 
 if ($ret->sendresult->error)
     echo "Ошибка №".$ret->sendresult->error;
-else {
+else 
     echo $ret->sendresult->id, "\n";
     echo $ret->sendresult->balance, "\n";
     echo $ret->sendresult->cost, "\n";
     echo $ret->sendresult->cnt, "\n";
 }
-
 ?>
